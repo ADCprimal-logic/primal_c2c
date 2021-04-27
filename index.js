aedc560169da3bc4170be4efcb5ad398f240e55e
anthony <anthony@primal-logic.io>
2021-04-27 13:55:01 +0000

    Added Schemas and Schema Folder

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const { GraphQLApp } = require("@keystonejs/app-graphql");
 const { AdminUIApp } = require("@keystonejs/app-admin-ui");
 const { NuxtApp } = require("@keystonejs/app-nuxt");
 const initialiseData = require("./initial-data");
+const userSchemas = require("./schemas/superadmin");
 // .ENV Configuration
 const dotenv = require("dotenv");
 dotenv.config();
@@ -34,55 +35,7 @@ const keystone = new Keystone({
   onConnect: process.env.CREATE_TABLES !== "true" && initialiseData,
 });
 
-// Access control functions
-const userIsAdmin = ({ authentication: { item: user } }) =>
-  Boolean(user && user.isAdmin);
-const userOwnsItem = ({ authentication: { item: user } }) => {
-  if (!user) {
-    return false;
-  }
-
-  // Instead of a boolean, you can return a GraphQL query:
-  // https://www.keystonejs.com/api/access-control#graphqlwhere
-  return { id: user.id };
-};
-
-const userIsAdminOrOwner = (auth) => {
-  const isAdmin = access.userIsAdmin(auth);
-  const isOwner = access.userOwnsItem(auth);
-  return isAdmin ? isAdmin : isOwner;
-};
-
-const access = { userIsAdmin, userOwnsItem, userIsAdminOrOwner };
-
-keystone.createList("User", {
-  fields: {
-    name: { type: Text },
-    email: {
-      type: Text,
-      isUnique: true,
-    },
-    isAdmin: {
-      type: Checkbox,
-      // Field-level access controls
-      // Here, we set more restrictive field access so a non-admin cannot make themselves admin.
-      access: {
-        update: access.userIsAdmin,
-      },
-    },
-    password: {
-      type: Password,
-    },
-  },
-  // List-level access controls
-  access: {
-    read: access.userIsAdminOrOwner,
-    update: access.userIsAdminOrOwner,
-    create: access.userIsAdmin,
-    delete: access.userIsAdmin,
-    auth: true,
-  },
-});
+exports.keystone = keystone;
 
 const authStrategy = keystone.createAuthStrategy({
   type: PasswordAuthStrategy,
