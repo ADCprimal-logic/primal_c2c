e357e1359b128daf33c8a510aec1d9293749e9d6
anthony <anthony@primal-logic.io>
2021-04-29 19:09:15 +0000

    Edit Schema

--- server/schemas/staff.js
+++ server/schemas/staff.js
@@ -2,27 +2,6 @@ const index = require("../index");
 const { Text, Checkbox, Password } = require("@keystonejs/fields");
 const keystone = index.indexKey;
 
-// Access control functions
-const userIsAdmin = ({ authentication: { item: user } }) =>
-  Boolean(user && user.isAdmin);
-const userOwnsItem = ({ authentication: { item: user } }) => {
-  if (!user) {
-    return false;
-  }
-
-  // Instead of a boolean, you can return a GraphQL query:
-  // https://www.keystonejs.com/api/access-control#graphqlwhere
-  return { id: user.id };
-};
-
-const userIsAdminOrOwner = (auth) => {
-  const isAdmin = access.userIsAdmin(auth);
-  const isOwner = access.userOwnsItem(auth);
-  return isAdmin ? isAdmin : isOwner;
-};
-
-const access = { userIsAdmin, userOwnsItem, userIsAdminOrOwner };
-
 console.log("Creating Staff");
 keystone.createList("StaffMember", {
   fields: {
@@ -34,8 +13,5 @@ keystone.createList("StaffMember", {
     },
     phone: { type: Text },
     status: { type: Text },
-    password: {
-      type: Password,
-    },
   },
 });
