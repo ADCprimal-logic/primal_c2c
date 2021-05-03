30a457db2013f729968674ec8d2b9a5cb372d4ca
samlemonsam <sam@primal-logic.io>
2021-05-03 12:26:17 -0400

    Starting the register page
    new_register

---
+++ client/src/pages/new_register.vue
@@ -0,0 +1,157 @@
+<template>
+  <div class="app">
+    <h1 class="main-heading">C2C Kids Registration</h1>
+    <p class="intro-text">
+      This form is for Kindergarten - 8th grade students at our Spartanburg or Greenville locations. If you would like to register for our Preschool in Lyman, please call (864) 529-9386 ext. 2 and request an appointment. Thank you!
+      <br />
+      <br />
+      <span>* </span>- Required Fields
+    </p>
+    <hr class="divider" />
+    <br />
+    <form>
+        <label><span>* </span>How did you hear about C2C Kids?</label>
+            <textarea id="hearAbout" name="hearAbout" rows="4" cols="50">
+            </textarea>
+        <br />
+        <br />
+        <label for="location"><span>* </span>C2C Location:</label>
+            <select name="cars" id="cars">
+                <option value="--">--</option>
+                <option value="GreenVille">Greenville</option>
+                <option value="Spartanburg">Spartanburg</option>
+            </select>
+        <br />
+        <br />
+            <input type="checkbox" id="vehicle1" name="vehicle1" value="Bike">
+            <label for="vehicle1"> I have a bike</label><br>
+
+            <input type="checkbox" id="vehicle2" name="vehicle2" value="Car">
+            <label for="vehicle2"> I have a car</label><br>
+
+            <input type="checkbox" id="vehicle3" name="vehicle3" value="Boat">
+            <label for="vehicle3"> I have a boat</label><br><br>
+        <br />
+        <br />
+        <input type="submit" value="Submit">
+    </form>
+  </div>
+</template>
+
+<style>
+*,
+*:before,
+*:after {
+  box-sizing: border-box;
+}
+body,
+html {
+  margin: 0;
+  padding: 0;
+}
+
+span {
+    color: red;    
+}
+body {
+  font-family: system-ui, BlinkMacSystemFont, -apple-system, Segoe UI, Roboto,
+    sans-serif;
+  display: flex;
+  justify-content: center;
+  min-height: 100vh;
+}
+
+.add-item::placeholder {
+  color: hsla(220, 30%, 60%, 0.8);
+}
+.add-item:focus {
+  outline: solid 2px hsl(220, 70%, 60%);
+}
+.remove-item:focus {
+  border-bottom: solid 2px hsl(220, 70%, 60%);
+}
+.remove-item {
+  background: transparent;
+  outline: none;
+  border: 0;
+  padding: 0;
+  cursor: pointer;
+}
+
+.app {
+  margin: 20px 20px;
+  padding: 20px;
+  max-width: 600px;
+  background: #fff;
+  border-radius: 18px;
+  border: solid 3px hsl(220, 70%, 60%, 0.8);
+  color: hsla(220, 84%, 14%, 1);
+}
+
+@media (min-width: 550px) {
+  .app {
+    margin: 10vh 10vw;
+    padding: 50px;
+  }
+}
+
+.main-heading {
+  font-weight: 900;
+}
+
+.intro-text {
+  line-height: 1.5;
+  color: hsla(220, 84%, 14%, 0.6);
+}
+
+.divider {
+  margin-top: 30px;
+  margin-left: 0;
+  width: 48px;
+  height: 4px;
+  border-width: 0;
+  background-color: hsla(220, 84%, 60%, 1);
+}
+
+.form-wrapper {
+  max-width: 500;
+}
+
+.app-heading {
+  text-transform: uppercase;
+  font-weight: 900;
+  margin-top: 50px;
+}
+
+.form-input {
+  color: hsla(220, 84%, 14%, 0.6);
+  padding: 12px 16px;
+  font-size: 1.25em;
+  width: 100%;
+  border-radius: 6;
+  border: 0;
+  border: solid 1px hsl(220, 30%, 70%);
+  background: hsl(220, 84%, 98%);
+}
+
+.list {
+  list-style: none;
+  padding: 0;
+}
+
+.list-item {
+  padding: 32px 16px;
+  font-size: 1.25em;
+  font-weight: 600;
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  border-top: 1px solid hsla(220, 84%, 60%, 0.32);
+}
+
+.delete-icon {
+  width: 20px;
+  height: 20px;
+  fill: hsla(220, 84%, 60%, 1);
+}
+</style>
