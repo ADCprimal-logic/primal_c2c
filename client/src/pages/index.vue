819654b03227523888bb3ef214875b2021fb9a82
anthony <anthony@primal-logic.io>
2021-05-10 17:14:40 +0000

    Set and Go

--- client/src/pages/index.vue
+++ client/src/pages/index.vue
@@ -1,8 +1,5 @@
 <template>
-  
-  
   <v-content>
-    
     <v-container fluid fill-height>
       <v-layout align-center justify-center>
         <v-flex xs12 sm8 md4>
@@ -13,18 +10,42 @@
           >
             <v-card-text>
               <v-form>
-                <v-text-field type="text" v-model="username" prepend-icon="person" name="username" label="Login" :placeholder="defaultUserPassword"></v-text-field>
-                <v-text-field type="password" v-model="password" prepend-icon="lock" name="password" label="Password" :placeholder="defaultUserPassword"></v-text-field>
+                <v-text-field
+                  type="text"
+                  v-model="username"
+                  prepend-icon="person"
+                  name="username"
+                  label="Login"
+                  :placeholder="defaultUserPassword"
+                ></v-text-field>
+                <v-text-field
+                  type="password"
+                  v-model="password"
+                  prepend-icon="lock"
+                  name="password"
+                  label="Password"
+                  :placeholder="defaultUserPassword"
+                ></v-text-field>
               </v-form>
             </v-card-text>
             <v-card-actions>
               <v-layout justify-center align-center>
-                <v-btn color="blue" :disabled="isDisabled" @click.prevent="authenticate">Login</v-btn>
+                <v-btn
+                  color="blue"
+                  :disabled="isDisabled"
+                  @click.prevent="authenticate"
+                  >Login</v-btn
+                >
               </v-layout>
             </v-card-actions>
             <v-card-actions>
               <v-layout justify-center align-center>
-                <v-btn color="blue" :disabled="isDisabled" @click.prevent="authenticate">Forgot Password?</v-btn>
+                <v-btn
+                  color="blue"
+                  :disabled="isDisabled"
+                  @click.prevent="authenticate"
+                  >Forgot Password?</v-btn
+                >
               </v-layout>
             </v-card-actions>
           </material-card>
@@ -35,36 +56,39 @@
 </template>
 
 <script>
-  import { mapActions } from 'vuex'
-  import materialCard  from '~/components/material/AppCard'
+import { mapActions } from "vuex";
+import materialCard from "~/components/material/AppCard";
 
-  export default {
-    components: {
-      materialCard
+export default {
+  components: {
+    materialCard,
+  },
+  data() {
+    return {
+      username: "email",
+      password: "admin",
+      defaultUserPassword: "admin",
+    };
+  },
+  computed: {
+    isDisabled() {
+      return (
+        this.username !== this.defaultUserPassword ||
+        this.password !== this.defaultUserPassword
+      );
     },
-    data() {
-      return {
-        username: 'admin',
-        password: 'admin',
-        defaultUserPassword: 'admin'
-      }
-    },
-    computed: {
-      isDisabled() {
-        return this.username !== this.defaultUserPassword || this.password !== this.defaultUserPassword;
-      }
-    },
-    methods: {
-      ...mapActions({
-        setUsername: 'user/setUsername'
-      }),
+  },
+  methods: {
+    ...mapActions({
+      setUsername: "user/setUsername",
+    }),
 
-      async authenticate() {
-        if (!this.isDisabled) {
-          await this.setUsername(this.defaultUserPassword);
-          this.$router.push({ path: 'dashboard' });
-        }
+    async authenticate() {
+      if (!this.isDisabled) {
+        await this.setUsername(this.defaultUserPassword);
+        this.$router.push({ path: "dashboard" });
       }
-    }
-  }
+    },
+  },
+};
 </script>
