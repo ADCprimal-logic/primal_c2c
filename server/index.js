6bd7a6f5c46725642dc7d0c50b673c4d1eac1873
anthony <anthony@primal-logic.io>
2021-05-28 18:17:35 +0000

    Edits based on the meetings notes

--- server/index.js
+++ server/index.js
@@ -73,6 +73,7 @@ const approvedContactSchema = require("./schema/approvedcontact");
 const childSchema = require("./schema/child");
 // Object Schemas
 const locationSchema = require("./schema/location");
+const roomSchema = require("./schema/room");
 const scheduleSchema = require("./schema/schedule");
 const healthSchema = require("./schema/health");
 const childCheckInSchema = require("./schema/childcheckin");
