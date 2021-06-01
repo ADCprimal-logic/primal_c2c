9a4428c66f070652b0a4f7f6684b1ae1e1aea229
anthony <anthony@primal-logic.io>
2021-06-01 14:05:58 +0000

    Fix

--- server/schema/schedule.js
+++ server/schema/schedule.js
@@ -8,7 +8,7 @@ keystone.createList("Schedule", {
     type: { type: Select, options: "Staff, Child" },
     basedIn: {
       type: Relationship,
-      ref: "Location.name",
+      ref: "Location",
       many: false,
       isRequired: true,
     },
