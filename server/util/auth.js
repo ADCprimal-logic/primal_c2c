152dcaefb48ef4a8f782881146ce39d465e4e6c6
anthony <anthony@primal-logic.io>
2021-06-10 17:34:34 +0000

    Toaster with Eyes

--- server/util/auth.js
+++ server/util/auth.js
@@ -1,10 +1,6 @@
 const index = require("../index");
 // DB Package Connection
 const postgres = require("postgres");
-// SMS Integration
-const twilio = require("twilio");
-// Random String Generator
-const crypto = require("crypto");
 // Password Hashing
 const bcrypt = require("bcrypt");
 // Initialize .ENV
