083280251737b1b49538ed71581193a047ba1f7f
cooperc2606 <chris.cooper@primal-logic.io>
2021-06-14 12:25:05 -0400

    Update stable release of news letter
    Created nested table to edit form fields for the news letter

--- client/src/pages/Admin-Newsletter.vue
+++ client/src/pages/Admin-Newsletter.vue
@@ -57,15 +57,103 @@
         <template v-slot:expand="props">
         <v-responsive :aspect-ratio="16 / 9">
         
+ 
         <v-textarea
           outlined
-          name="input-7-4"
-          label="Subject"
-          value="This is a test area for the content of the news letter."
+          name="Content"
+          label="News Letter Conent"
+          :value= props.item.content
           disabled
-          class = "justify-center"
-        ></v-textarea>
-            
+        > 
+        </v-textarea>
+        <v-btn disabled block color="black">Edit Newsletter</v-btn>
+           <v-card
+            class="mx-auto"
+            style="max-width: 500px;"
+          >
+    <v-form
+      ref="form"
+      v-model="form"
+      class="pa-4 pt-6"
+    >
+      <v-text-field
+        color="C2Cblue"
+        counter="60"
+        filled
+        round
+        label="Subject"
+        :value= props.item.subject
+        style="min-height: 96px"
+      ></v-text-field>
+        <v-textarea
+          outlined
+          name="Content"
+          label="Enter content here"
+          :value= props.item.content
+        > 
+        </v-textarea>
+      <v-text-field
+        color="C2Cblue"
+        label="Update Date Posted"
+        :value= props.item.date_posted
+      ></v-text-field>
+      <v-textarea
+        auto-grow
+        filled
+        color="C2Cblue"
+        label="Update Time Posted"
+        :value= props.item.time_posted
+        rows="1"
+      ></v-textarea>
+    </v-form>
+    <v-divider></v-divider>
+    <v-card-actions>
+      <v-btn
+        text
+        @click="$refs.form.reset()"
+      >
+        Clear
+      </v-btn>
+      <v-spacer></v-spacer>
+      <v-btn
+        :disabled="!form"
+        :loading="isLoading"
+        class="white--text"
+        color="orange"
+        depressed
+      >Submit</v-btn>
+    </v-card-actions>
+    <v-dialog
+      v-model="dialog"
+      absolute
+      max-width="400"
+      persistent
+    >
+      <v-card>
+        <v-card-title class="headline grey lighten-3">Legal</v-card-title>
+        <v-card-text>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </v-card-text>
+        <v-divider></v-divider>
+        <v-card-actions>
+          <v-btn
+            text
+            @click="agreement = false, dialog = false"
+          >
+            No
+          </v-btn>
+          <v-spacer></v-spacer>
+          <v-btn
+            class="white--text"
+            color="deep-purple accent-4"
+            @click="agreement = true, dialog = false"
+          >
+            Yes
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </v-card>  
         
                 
                     
