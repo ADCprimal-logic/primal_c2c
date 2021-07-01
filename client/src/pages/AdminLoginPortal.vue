4efa88abcb380005a71458536aab4d6d47e0c310
anthony <anthony@primal-logic.io>
2021-07-01 20:27:44 +0000

    Set Right URL

--- client/src/pages/AdminLoginPortal.vue
+++ client/src/pages/AdminLoginPortal.vue
@@ -97,7 +97,7 @@
 
 <script>
     function login(data) {
-        return fetch("http://localhost:3000/api/auth/login/", {
+        return fetch("http://100.25.45.227:3000/api/auth/login/", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
@@ -112,7 +112,7 @@
     }
 
     function getUser(data) {
-        return fetch("http://localhost:3000/api/auth/user/", {
+        return fetch("http://100.25.45.227:3000/api/auth/user/", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
--- client/src/pages/index.vue
+++ client/src/pages/index.vue
@@ -72,7 +72,7 @@
 
 <script>
     function login(data) {
-        return fetch("http://localhost:3000/api/auth/login/", {
+        return fetch("http://100.25.45.227:3000/api/auth/login/", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
@@ -87,7 +87,7 @@
     }
 
     function getUser(data) {
-        return fetch("http://localhost:3000/api/auth/user/", {
+        return fetch("http://100.25.45.227:3000/api/auth/user/", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
