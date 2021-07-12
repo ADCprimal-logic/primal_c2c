83553c73cc322c570359d7364c89489d85da264a
anthony <anthony@primal-logic.io>
2021-07-12 13:53:33 +0000

    HARD Reset

--- client/src/store/app/state.js
+++
@@ -1,5 +0,0 @@
-export default () => ({
-  drawer: null,
-  color: 'info',
-  image: 'https://primal-c2c-images.s3.amazonaws.com/static/bluetheme.jpg'
-})
--- server/server-ui/react-components/logo.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-export default () => {
-  return (
-    <>
-      <img src={"vuetifylogo.png"} />
-    </>
-  );
-};
