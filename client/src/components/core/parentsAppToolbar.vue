db353cd858335fa4c855b8dcda9e0011de73d708
samlemonsam <sam@primal-logic.io>
2021-07-14 12:18:02 -0400

    Fixed Titles for Parent Toolbars

--- client/src/components/core/parentsAppToolbar.vue
+++ client/src/components/core/parentsAppToolbar.vue
@@ -1,98 +1,171 @@
 <template>
-  <v-toolbar
-    id="core-toolbar"
-    flat
-    prominent
-    style="background: #eee;"
-  >
-    <div class="v-toolbar-title">
-      <v-toolbar-title
-        class="tertiary--text font-weight-light"
-      >
-        <v-btn
-          v-if="responsive"
-          class="default v-btn--simple"
-          dark
-          icon
-          @click.stop="onClickBtn"
-        >
-          <v-icon>mdi-view-list</v-icon>
-        </v-btn>
-        {{ title }}
-      </v-toolbar-title>
-    </div>
+    <v-toolbar id="core-toolbar"
+               flat
+               prominent
+               style="background: #eee;">
+        <div class="v-toolbar-title" v-if="title === 'Parents-Dashboard' || title === ''">
+            <v-toolbar-title class="tertiary--text font-weight-light">
+                <v-btn v-if="responsive"
+                       class="default v-btn--simple"
+                       dark
+                       icon
+                       @click.stop="onClickBtn">
+                    <v-icon>mdi-view-list</v-icon>
+                </v-btn>
+                Parent Dashboard
+            </v-toolbar-title>
+        </div>
+        <div class="v-toolbar-title" v-else-if="title === 'Parent-Schedule'">
+            <v-toolbar-title class="tertiary--text font-weight-light">
+                <v-btn v-if="responsive"
+                       class="default v-btn--simple"
+                       dark
+                       icon
+                       @click.stop="onClickBtn">
+                    <v-icon>mdi-view-list</v-icon>
+                </v-btn>
+                Schedule
+            </v-toolbar-title>
+        </div>
+        <div class="v-toolbar-title" v-else-if="title === 'Parents-Profile-Editor'">
+            <v-toolbar-title class="tertiary--text font-weight-light">
+                <v-btn v-if="responsive"
+                       class="default v-btn--simple"
+                       dark
+                       icon
+                       @click.stop="onClickBtn">
+                    <v-icon>mdi-view-list</v-icon>
+                </v-btn>
+                Parent Profile Editor
+            </v-toolbar-title>
+        </div>
+        <div class="v-toolbar-title" v-else-if="title === 'Parents-Approved-Contacts'">
+            <v-toolbar-title class="tertiary--text font-weight-light">
+                <v-btn v-if="responsive"
+                       class="default v-btn--simple"
+                       dark
+                       icon
+                       @click.stop="onClickBtn">
+                    <v-icon>mdi-view-list</v-icon>
+                </v-btn>
+                Approved Contacts Page
+            </v-toolbar-title>
+        </div>
+        <div class="v-toolbar-title" v-else-if="title === 'Parents-Faculty-Contact'">
+            <v-toolbar-title class="tertiary--text font-weight-light">
+                <v-btn v-if="responsive"
+                       class="default v-btn--simple"
+                       dark
+                       icon
+                       @click.stop="onClickBtn">
+                    <v-icon>mdi-view-list</v-icon>
+                </v-btn>
+                Faculty Contact Page
+            </v-toolbar-title>
+        </div>
+        <div class="v-toolbar-title" v-else-if="title === 'Parents-Lunch-Portal'">
+            <v-toolbar-title class="tertiary--text font-weight-light">
+                <v-btn v-if="responsive"
+                       class="default v-btn--simple"
+                       dark
+                       icon
+                       @click.stop="onClickBtn">
+                    <v-icon>mdi-view-list</v-icon>
+                </v-btn>
+                Lunch Portal
+            </v-toolbar-title>
+        </div>
+        <div class="v-toolbar-title" v-else-if="title === 'Parents-Child-Registration'">
+            <v-toolbar-title class="tertiary--text font-weight-light">
+                <v-btn v-if="responsive"
+                       class="default v-btn--simple"
+                       dark
+                       icon
+                       @click.stop="onClickBtn">
+                    <v-icon>mdi-view-list</v-icon>
+                </v-btn>
+                Child Re-Enrollment
+            </v-toolbar-title>
+        </div>
+        <div class="v-toolbar-title" v-else-if="title === 'slack-parent'">
+            <v-toolbar-title class="tertiary--text font-weight-light">
+                <v-btn v-if="responsive"
+                       class="default v-btn--simple"
+                       dark
+                       icon
+                       @click.stop="onClickBtn">
+                    <v-icon>mdi-view-list</v-icon>
+                </v-btn>
+                C2C Messenger
+            </v-toolbar-title>
+        </div>
+        <div class="v-toolbar-title" v-else>
+            <v-toolbar-title class="tertiary--text font-weight-light">
+                <v-btn v-if="responsive"
+                       class="default v-btn--simple"
+                       dark
+                       icon
+                       @click.stop="onClickBtn">
+                    <v-icon>mdi-view-list</v-icon>
+                </v-btn>
+                {{ title }}
+            </v-toolbar-title>
+        </div>
 
-    <v-spacer />
-    <v-toolbar-items>
-      <v-flex
-        align-center
-        layout
-        py-2
-      >
-        <nuxt-link
-          v-ripple
-          class="toolbar-items"
-          to="/Parents-Dashboard"
-          title="Dashboard"
-        >
-          <v-icon color="tertiary">mdi-view-dashboard</v-icon>
-        </nuxt-link>
-        <v-menu
-          bottom
-          left
-          content-class="dropdown-menu"
-          offset-y
-          transition="slide-y-transition">
-          <router-link
-            v-ripple
-            slot="activator"
-            class="toolbar-items"
-            to="/Parents-Inbox"
-          >
-            <v-badge
-              color="error"
-              overlap
-            >
-              <template slot="badge">
-                {{ notifications.length }}
-              </template>
-              <v-icon color="tertiary">mdi-bell</v-icon>
-            </v-badge>
-          </router-link>
-          <v-card>
-            <v-list dense>
-              <v-list-tile
-                v-for="notification in notifications"
-                :key="notification"
-                @click="onClick"
-              >
-                <v-list-tile-title
-                  v-text="notification"
-                />
-              </v-list-tile>
-            </v-list>
-          </v-card>
-        </v-menu>
-        <nuxt-link
-          v-ripple
-          class="toolbar-items"
-          to="/Parents-Child-Profile-Editor"
-          title="User profile"
-        >
-          <v-icon color="tertiary">mdi-account</v-icon>
-        </nuxt-link>
-        <nuxt-link
-          v-ripple
-          class="toolbar-items"
-          to="/"
-          title="Logout"
-          @click.native="logout"
-        >
-          <v-icon color="tertiary">mdi-logout</v-icon>
-        </nuxt-link>
-      </v-flex>
-    </v-toolbar-items>
-  </v-toolbar>
+        <v-spacer />
+        <v-toolbar-items>
+            <v-flex align-center
+                    layout
+                    py-2>
+                <nuxt-link v-ripple
+                           class="toolbar-items"
+                           to="/Parents-Dashboard"
+                           title="Dashboard">
+                    <v-icon color="tertiary">mdi-view-dashboard</v-icon>
+                </nuxt-link>
+                <v-menu bottom
+                        left
+                        content-class="dropdown-menu"
+                        offset-y
+                        transition="slide-y-transition">
+                    <router-link v-ripple
+                                 slot="activator"
+                                 class="toolbar-items"
+                                 to="/Parents-Inbox">
+                        <v-badge color="error"
+                                 overlap>
+                            <template slot="badge">
+                                {{ notifications.length }}
+                            </template>
+                            <v-icon color="tertiary">mdi-bell</v-icon>
+                        </v-badge>
+                    </router-link>
+                    <v-card>
+                        <v-list dense>
+                            <v-list-tile v-for="notification in notifications"
+                                         :key="notification"
+                                         @click="onClick">
+                                <v-list-tile-title v-text="notification" />
+                            </v-list-tile>
+                        </v-list>
+                    </v-card>
+                </v-menu>
+                <nuxt-link v-ripple
+                           class="toolbar-items"
+                           to="/Parents-Child-Profile-Editor"
+                           title="User profile">
+                    <v-icon color="tertiary">mdi-account</v-icon>
+                </nuxt-link>
+                <nuxt-link v-ripple
+                           class="toolbar-items"
+                           to="/"
+                           title="Logout"
+                           @click.native="logout">
+                    <v-icon color="tertiary">mdi-logout</v-icon>
+                </nuxt-link>
+            </v-flex>
+        </v-toolbar-items>
+    </v-toolbar>
 </template>
 
 <script>
